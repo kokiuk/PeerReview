b5fb77e24d6ee15169ffc24c921667caa55d1cb1
kokiuk <kgukid38@naver.com>
2024-01-12 13:34:48 +0900

    update

--- kiuk/mini/src/main/java/com/example/mini/hash/repo/HashRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.mini.hash.repo;
-
-import com.example.mini.hash.entity.Hash;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HashRepo extends JpaRepository<Hash, Long> {
-    public Hash findByHashWord(String hashWord);
-}
