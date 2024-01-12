8299f727dc3e3e67d5a0fd95628c626a71e1fb69
kokiuk <kgukid38@naver.com>
2024-01-12 13:09:15 +0900

    수정

--- mini/src/main/java/com/example/mini/Image/repo/ImageRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.mini.Image.repo;
-
-import com.example.mini.Image.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepo extends JpaRepository<Image, Long> {
-}
